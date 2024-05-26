d202361167a014cde2ca27fbc8278558a5e9a6a2
rani93 <rani931849@gmail.com>
2024-05-27 01:08:22 +0530

    combine two components

---
+++ week1/functionComponentUsingArrowFunction/index.js
@@ -0,0 +1,20 @@
+const Sum =()=>{
+    return(<>
+    <h1>hello</h1>
+    <p>Rani !!</p>
+    </>)
+}
+
+const Name=()=>{
+    return(<>
+    <h1>it's me </h1></>)
+}
+ReactDOM.createRoot(document.getElementById("root")).render(
+<>
+    <Sum/>
+    <Name/>
+    
+    </>
+);
+
+
