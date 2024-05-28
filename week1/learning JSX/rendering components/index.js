594ddc674c47278dbfba86228a64723ffe6211ca
rani93 <rani931849@gmail.com>
2024-05-28 14:12:58 +0530

    Components Rendering

---
+++ week1/learning JSX/rendering components/index.js
@@ -0,0 +1,114 @@
+function Students(){
+    let student =[
+        {
+            name:"rani",
+            class:"10",
+            rollno:"2"
+        },
+        {
+            name:"rani singh",
+            class:"11",
+            rollno:"23"
+        },
+        {
+            name:"rani kumari",
+            class:"110",
+            rollno:"253"
+        }
+
+    ];
+    return(
+        <>
+        <h1>
+            Student Details
+        </h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>class</th>
+                <th>rollno</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                student.map((stu)=>
+                    (
+                        <tr>
+                            <td>{stu.name}</td>
+                            <td>{stu.class}</td>
+                            <td>{stu.rollno}</td>
+                        </tr>
+                    )
+
+                )
+            }
+            </tbody>
+
+        </table>
+        </>
+    );
+}
+function Cars(){
+    let car =[
+        {
+            name:"maruti",
+            class:"101",
+            rollno:"274"
+        },
+        {
+            name:"sony",
+            class:"11",
+            rollno:"23"
+        },
+        {
+            name:"honda",
+            class:"110",
+            rollno:"253"
+        }
+
+    ];
+    return(
+        <>
+        <h1>
+          Cars Details
+        </h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>quantity</th>
+                <th>number</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                car.map((stu)=>
+                    (
+                        <tr>
+                            <td>{stu.name}</td>
+                            <td>{stu.class}</td>
+                            <td>{stu.rollno}</td>
+                        </tr>
+                    )
+
+                )
+            }
+            </tbody>
+
+        </table>
+        </>
+    );
+}
+
+function App(){
+    return(
+        <>
+        <Students/>
+        <Cars/>
+        </>
+        
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
