4fee9c4c915157f11702f30bcd261837ce84e585
rani93 <rani931849@gmail.com>
2024-05-29 11:41:27 +0530

    new

--- week2/score-keeper App/index.js
+++ week2/score-keeper App/index.js
@@ -1,9 +1,12 @@
 let score = 0;
 let wicket = 0;
+let ballWiseResult =[];
 
 function addSum(num){
     if(wicket<10)
-        {score+=num;
+        {
+            ballWiseResult.push(num);
+            score+=num;
             element.render(<App/>)
         }
     
@@ -13,26 +16,36 @@ function addSum(num){
 
 function addWicket(){
     if(wicket<10)
-    {wicket+=1;
+    {
+        ballWiseResult.push("W");
+        wicket+=1;
         element.render(<App/>)
     }
     
 }
-const App=()=>(
-    <>
-    <h1>SCORE KEEPER</h1>
-    <h2>SCORE:{score}/{wicket}</h2>
+
+const  ScoreButton = ()=>(
+
     <div>
         <button onClick={()=>{addSum(1)}}>1</button>
         <button onClick={()=>{addSum(2)}}>2</button>
-        <button onClick={()=>{addSum(3)}}> 3</button>
+        <button onClick={()=>{addSum(3)}}>3</button>
         <button onClick={()=>{addSum(4)}}>4</button>
         <button onClick={()=>{addSum(5)}}>5</button>
         <button onClick={()=>{addSum(6)}}>6</button>
         <button onClick={()=>{addWicket()}}>wicket</button>
 
     </div>
+    
+)
+const App=()=>(
+    <>
+    <h1>SCORE KEEPER</h1>
+    <h2>SCORE:{score}/{wicket}</h2>
+    <ScoreButton/>
     </>
+    
+
 )
 const element =ReactDOM.createRoot(document.getElementById("root"));
 element.render(<App/>)
