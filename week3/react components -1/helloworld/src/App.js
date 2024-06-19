3e743797ba45654e5c809c5d16eb5313141d02c5
rani93 <rni931849@gmail.com>
2024-06-19 16:22:38 +0530

    updated

---
+++ week3/react components -1/helloworld/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+class App extends React.Component{
+  render(){
+    return(
+    <>  
+    <ul>
+      <li>Note</li>
+      <li>render function is only present in class components</li>
+      <li>render function is used to return JSX</li>
+      <li>render function is called automatically when the component is rendered</li>
+      
+         </ul>
+      <h1>Class component</h1>
+      </>
+
+    )
+  }
+
+}
+
+export default App;
